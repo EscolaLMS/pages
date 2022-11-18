cdabc98251bc43606eca3f407f6fc3420b04d9d7
Maciej Rymarz <59456825+mako321@users.noreply.github.com>
2022-11-18 16:30:07 +0100

    Add translations (#32)

---
+++ resources/lang/en/permissions.php
@@ -0,0 +1,11 @@
+<?php
+
+use EscolaLms\Pages\Enums\PagesPermissionsEnum;
+
+return [
+    PagesPermissionsEnum::PAGE_LIST => 'Page list',
+    PagesPermissionsEnum::PAGE_READ => 'Read page',
+    PagesPermissionsEnum::PAGE_CREATE => 'Create page',
+    PagesPermissionsEnum::PAGE_DELETE => 'Delete page',
+    PagesPermissionsEnum::PAGE_UPDATE => 'Update page',
+];
---
+++ resources/lang/pl/permissions.php
@@ -0,0 +1,11 @@
+<?php
+
+use EscolaLms\Pages\Enums\PagesPermissionsEnum;
+
+return [
+    PagesPermissionsEnum::PAGE_LIST => 'Lista stron',
+    PagesPermissionsEnum::PAGE_READ => 'Wyświetl stronę',
+    PagesPermissionsEnum::PAGE_CREATE => 'Utwórz stronę',
+    PagesPermissionsEnum::PAGE_DELETE => 'Usuń stronę',
+    PagesPermissionsEnum::PAGE_UPDATE => 'Aktualizuj stronę',
+];
